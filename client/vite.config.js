2678e86586b14f62ac216326e696add8db777a27
Nithiswaran <nithiswarankrish@gmail.com>
2026-08-06 21:15:55 +0530

    Set base path to /Repository-name-book-a-doctor/ in vite.config.js to resolve 404 blank screen on GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Repository-name-book-a-doctor/',
   plugins: [react()],
   server: {
     port: 3000,
