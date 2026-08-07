f40f50494a7b15058ae354f0f4bd14401ffc8f68
Nithiswaran <nithiswarankrish@gmail.com>
2026-08-07 16:29:56 +0530

    Fix Vite base path for Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Repository-name-book-a-doctor/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
